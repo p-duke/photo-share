a355ad112d5822241e9773c2829c6c789bc1e963
p-duke <peteraaronduke@gmail.com>
2025-01-20 20:35:52 -0500

    Create basic router

--- main.go
+++ main.go
@@ -5,6 +5,19 @@ import (
 	"net/http"
 )
 
+type Router struct{}
+
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		http.Error(w, "Page not found", http.StatusNotFound)
+	}
+}
+
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
@@ -13,12 +26,30 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 func contactHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:jon@calhoun.io\">jon@calhoun.io</a>.</p>")
+}
 
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, r.URL.Path)
+	if r.URL.Path == "/" {
+		homeHandler(w, r)
+	} else if r.URL.Path == "/contact" {
+		contactHandler(w, r)
+	}
+
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		// Todo: handle the page not found error
+		w.WriteHeader(http.StatusNotFound)
+		http.Error(w, "Page not found", http.StatusNotFound)
+	}
 }
 
 func main() {
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/contact", contactHandler)
+	var router Router
 	fmt.Println("Starting the server on :3000...")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", router)
 }
