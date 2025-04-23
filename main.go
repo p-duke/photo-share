0bb9185de3bd45713ee344754d208bc43cd31ac8
p-duke <peteraaronduke@gmail.com>
2025-04-23 17:21:52 -0400

    add must functions

--- main.go
+++ main.go
@@ -66,23 +66,13 @@ func pathHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	r := chi.NewRouter()
 
-	tpl, err := views.Parse(filepath.Join("templates", "home.gohtml"))
-	if err != nil {
-		panic(err)
-	}
+	tpl := views.Must(views.Parse(filepath.Join("templates", "home.gohtml")))
 	r.Get("/", controllers.StaticHandler(tpl))
 
-
-	tpl, err = views.Parse(filepath.Join("templates", "contact.gohtml"))
-	if err != nil {
-		panic(err)
-	}
+	tpl = views.Must(views.Parse(filepath.Join("templates", "contact.gohtml")))
 	r.Get("/contact", controllers.StaticHandler(tpl))
 
-	tpl, err = views.Parse(filepath.Join("templates", "faq.gohtml"))
-	if err != nil {
-		panic(err)
-	}
+	tpl = views.Must(views.Parse(filepath.Join("templates", "faq.gohtml")))
 	r.Get("/faq", controllers.StaticHandler(tpl))
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
@@ -92,3 +82,4 @@ func main() {
 	fmt.Println("Starting the server on :3000...")
 	http.ListenAndServe(":3000", r)
 }
+
--- views/template.go
+++ views/template.go
@@ -17,6 +17,13 @@ func Parse(filepath string) (Template, error) {
 	}, nil
 }
 
+func Must(t Template, err error) Template {
+	if err != nil {
+		panic(err)
+	}
+	return t
+}
+
 type Template struct {
 	htmlTpl *template.Template
 }
